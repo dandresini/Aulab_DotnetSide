a79f22327daff7981962f6963172fd2a23e3686a
Damiano Andresini <d.andresini@cotrap.it>
2022-09-16 17:25:48 +0200

    Add project files.

---
+++ Aulab/Client/Program.cs
@@ -0,0 +1,23 @@
+using Aulab.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("Aulab.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Aulab.ServerAPI"));
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://damianoandresinioutlookit.onmicrosoft.com/678c2508-f8ba-4877-87f3-3bd7ec3cae84/api.read");
+    options.ProviderOptions.LoginMode = "redirect";
+});
+
+await builder.Build().RunAsync();
---
+++ AulabB2C/Client/Program.cs
@@ -0,0 +1,26 @@
+using AulabB2C.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("AulabB2C.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("AulabB2C.ServerAPI"));
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureB2C", options.ProviderOptions.Authentication);
+
+    options.ProviderOptions.DefaultAccessTokenScopes.Add($"https://{builder.Configuration["AzureB2C:TenantName"]}/backendapi/api.write");
+    options.ProviderOptions.AdditionalScopesToConsent.Add($"https://{builder.Configuration["AzureB2C:TenantName"]}/backendapi/api.read");
+    options.ProviderOptions.LoginMode = "redirect";
+
+});
+
+await builder.Build().RunAsync();
